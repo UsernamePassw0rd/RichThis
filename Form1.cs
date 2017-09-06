e22e948a2a41b7cbec61eac81553304577a4b24b
UsernamePassw0rd <baphometse@gmail.com>
2017-09-06 12:14:56 +0700

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RichThis
+{
+    public partial class frmRichThis : Form
+    {
+        public frmRichThis()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void frmRichThis_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
